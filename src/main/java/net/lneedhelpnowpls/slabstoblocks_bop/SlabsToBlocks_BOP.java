d66528298a8060caa2f9d9070bccec70728f9943
LneedhelpnowPLS <marcushadriantextor@gmail.com>
2025-03-01 16:32:25 -0500

    Fixed version and removed comments

--- src/main/java/net/lneedhelpnowpls/slabstoblocks_bop/SlabsToBlocks_BOP.java
+++ src/main/java/net/lneedhelpnowpls/slabstoblocks_bop/SlabsToBlocks_BOP.java
@@ -1,5 +1,4 @@
 package net.lneedhelpnowpls.slabstoblocks_bop;
-
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -10,13 +9,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(SlabsToBlocks_BOP.MOD_ID)
 public class SlabsToBlocks_BOP {
     public static final String MOD_ID = "slabstoblocks_bop";
     private static final Logger LOGGER = LogUtils.getLogger();
-
     public SlabsToBlocks_BOP() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
@@ -24,12 +20,8 @@ public class SlabsToBlocks_BOP {
 
         MinecraftForge.EVENT_BUS.register(this);
     }
-
     private void commonSetup(final FMLCommonSetupEvent event) {
-
     }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
