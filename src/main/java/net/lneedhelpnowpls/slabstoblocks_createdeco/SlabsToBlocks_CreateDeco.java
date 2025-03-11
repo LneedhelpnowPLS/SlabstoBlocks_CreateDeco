d53ffcd92d6194e3115e11cec78f772bcd7d772a
LneedhelpnowPLS <marcushadriantextor@gmail.com>
2025-03-10 23:22:07 -0400

    Removing many crafting recipes from STB_BOP, made example recipes for later, modified mod id, and probably more but got no clue since I'm just mentally drained atm

--- src/main/java/net/lneedhelpnowpls/slabstoblocks_createdeco/SlabsToBlocks_CreateDeco.java
+++ src/main/java/net/lneedhelpnowpls/slabstoblocks_createdeco/SlabsToBlocks_CreateDeco.java
@@ -1,4 +1,4 @@
-package net.lneedhelpnowpls.slabstoblocks_bop;
+package net.lneedhelpnowpls.slabstoblocks_createdeco;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -9,11 +9,11 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-@Mod(SlabsToBlocks_BOP.MOD_ID)
-public class SlabsToBlocks_BOP {
-    public static final String MOD_ID = "slabstoblocks_bop";
+@Mod(SlabsToBlocks_CreateDeco.MOD_ID)
+public class SlabsToBlocks_CreateDeco {
+    public static final String MOD_ID = "slabstoblocks_createdeco";
     private static final Logger LOGGER = LogUtils.getLogger();
-    public SlabsToBlocks_BOP() {
+    public SlabsToBlocks_CreateDeco() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
